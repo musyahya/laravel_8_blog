96464d78272f1b2daa77f65c7294763f92f04bc1
musyahya <mananabdul37654@gmail.com>
2021-02-12 19:33:22 +0700

    final reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -34,7 +34,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Reset Password') }}
                                 </button>
                             </div>
                         </div>

97bab105ce9a49ef2676080b242b94a901a0e142
musyahya <mananabdul37654@gmail.com>
2021-03-03 15:33:09 +0700

    final_komentar

--- resources/views/artikel/artikel.blade.php
+++ resources/views/artikel/artikel.blade.php
@@ -30,4 +30,24 @@
             <a href="/like/{{$artikel->id}}" class="text-danger"><i class="fas fa-heart"></i> {{$like}} Like</a>
         </div>
     </div>
+
+    <div id="disqus_thread" class="mt-4"></div>
+    <script>
+        /**
+        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+        /*
+        var disqus_config = function () {
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+        */
+        (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://blog-wwe7ssfgas.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 @endsection
